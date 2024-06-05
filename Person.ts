fa6edf16f2246f9dce0752b9c24cb1e78c76a101
asadullah48 <asadullahshafique@hotmail.com>
2024-06-05 10:31:16 +0500

    Initial commit

---
+++ Person.ts
@@ -0,0 +1,18 @@
+// Named export example in file: utils.ts
+export const utilOne = () => {
+    /*...*/
+  };
+  export const utilTwo = () => {
+    /*...*/
+  };
+
+  // In another file:
+//import { Person } from "./Person";
+
+//const asadullah = new Person("Asadullah Shafique");
+//asadullah.greet(); 
+
+
+// Outputs: Hello, my name is Alice
+// This snippet imports the Person 
+//class and uses it to create an instance.
---
+++ ans131.ts
@@ -0,0 +1,9 @@
+// In another file:
+import { Person } from "./Person";
+
+const asadullah = new Person("Asadullah Shafique");
+asadullah.greet(); // Outputs: Hello, my name is Alice
+// This snippet imports the Person 
+//class and uses it to create an instance.
+
+
